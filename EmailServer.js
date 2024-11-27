2af5433c76587a7914e47835fcc1774cf2d7ce9d
FAverruz <146257059+FAverruz@users.noreply.github.com>
2024-11-27 23:52:13 +0100

    cors update

--- EmailServer.js
+++ EmailServer.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: 'https://franciscoaverruz.netlify.app, http://localhost:5173/',
+  origin: '*',
   methods: 'POST',
   allowedHeaders: 'Content-Type'
 }));
